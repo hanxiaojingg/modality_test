da0f6ce10a68eb460d083b0e414a9dec992bf5d8
Hanxiao Jing <78436887+hanxiaojingg@users.noreply.github.com>
2025-12-10 15:11:05 -0600

    Update normal_error_modetest.R

--- normal_error_modetest.R
+++ normal_error_modetest.R
@@ -33,11 +33,6 @@ makegamma <- function(y_values, t0, d, h) {
   return(gamma)
 }
 
-t0=d
-t1=t0+d
-t2=t1+d
-t3=t2+d
-makegamma(y_values,t0,d)
 
 makec=function(y,tk,d,h){
   nc=length(y)
@@ -50,102 +45,73 @@ makec=function(y,tk,d,h){
 }
 
 
-
-nraw=length(y)
-# q1=quantile(y,.01)
-# q2=quantile(y,.99)
-# rng=q2-q1
-# s1=as.numeric(q1-.4*rng)
-# s2=as.numeric(q2+.4*rng)
-# l = round((10*n^(1/9))*h/(s2-s1))
-# l = 2
-# d = h/l 
-# ran=s2-s1
-# s1=s1-(ceiling(ran/d)*d-ran)/2
-# s2=s2+(ceiling(ran/d)*d-ran)/2
-s1=min(x)
-s2=max(x)
-tk = seq(s1,s2,length.out = round((10*n^(1/9))))
-d = tk[2]-tk[1]
-m = length(tk)-3
-yp = seq(s1-3*h,s2+3*h,length.out = 2000); np = length(yp)
-yraw = y
-y = y[y>=s1 & y<=s2]
-n = length(y)
-  K = kurtosis(y)
-  if(K<2){
-    lam = 10^2*n^(-1/7)
-  } else if(K>2 & K<5){
-    lam = 10^(4-K)*n^(-1/7)
-  } else if(K>5 & K<9){
-    lam = 10^(3/2-K/2)*n^(-1/7)
-  } else{
-    lam = 10^(-3)*n^(-1/7)
+deconmodetest <- function(y, h, v=0.01, lam=NULL, eps1=2, eps2=1, od=2/9, B=100){
+  nraw=length(y)
+  s1=min(y)
+  s2=max(y)
+  tk = seq(s1,s2,length.out = round((12*n^(1/9))))
+  d = tk[2]-tk[1]
+  m = length(tk)-3
+  yp = seq(s1-3*h,s2+3*h,length.out = 2000); np = length(yp)
+  yraw = y
+  y = y[y>=s1 & y<=s2]
+  n = length(y)
+  if(is.null(lam)){
+    K = kurtosis(y)
+    if(K<2){
+      lam = 10^2*n^(-1/7)
+    } else if(K>2 & K<5){
+      lam = 10^(4-K)*n^(-1/7)
+    } else if(K>5 & K<9){
+      lam = 10^(3/2-K/2)*n^(-1/7)
+    } else{
+      lam = 10^(-3)*n^(-1/7)
+    }
+    lam = v*lam
   }
-  v=0.01
-  lam = v*lam
-  eps1=2;eps2=1
   eps=ifelse(abs(skewness(y))>0.7,eps2,eps1)
-delta  = matrix(0,nrow=np,ncol=m)
-for(i in 1:m){
-  delta[yp>tk[i]&yp<=tk[i+1],i] = 2*(yp[yp>tk[i]&yp<=tk[i+1]]-tk[i])^2/d^2/3
-  delta[yp>=tk[i+1]&yp<=tk[i+2],i] = 1-4*(yp[yp>=tk[i+1]&yp<=tk[i+2]]-tk[i+1]-d/2)^2/d^2/3
-  delta[yp>tk[i+2]&yp<=tk[i+3],i] = 2*(yp[yp>tk[i+2]&yp<=tk[i+3]]-tk[i+3])^2/d^2/3
-}
-gamma = matrix(0,nrow=np,ncol=m)
-for(j in 1:m){
-  gamma[,j] = makegamma(yp,tk[j],d, h)
-}
-hmat = matrix(0,nrow=m,ncol=m)
-for(i in 1:m){
-  for(j in 1:i)
-    hmat[i,j] = hmat[j,i]=sum(gamma[,i]*gamma[,j])*(yp[2]-yp[1])
-}
-avec = 1:m*0+4*d/3
-wmat = matrix(0,nrow=m,ncol=m-1)
-for(i in 1:(m-1)){
-  wmat[i,i] = -1;wmat[i+1,i]=1
-}
-b0 = 1:m*0+3/m/d/4
-cvec = makec(y,tk,d,h)
-D <- matrix(0,m+1,m)
-for(i in 1:m){
-  D[i,i]=3
-  D[i+1,i]=-3
-}
-for (i in 1:(m-1)) D[i,i+1]=-1
-for(i in 1:(m-1)) D[i+2,i]=1
-D = 4*D/3*sqrt(d)
-##  get unimodal
-od=2/9
-ans1=deconumfit(y,tk,hmat,cvec,b0,wmat,D,lam,eps=eps,od)
-ans2=deconbmfit(y,tk,hmat,cvec,b0,wmat,D,lam)
-t1=as.numeric((ans1$crit-ans2$crit))
-#t2=as.numeric((ans1$crit-ans2$crit)/abs(ans1$crit))
-fhat1=round(delta%*%ans1$bhat,10)
-fhat2=round(delta%*%ans2$bhat,10)
-ghat1=round(gamma%*%ans1$bhat,10)
-ghat2=round(gamma%*%ans2$bhat,10)
-dfhat2=diff(fhat2)
-outtb=NULL
-par(mfrow=c(1,2))
-hist(x,breaks=30,freq=FALSE)
-lines(yp,fhat1,col=2)
-lines(yp,fhat2,col=3)
-lines(yp,0.6*dnorm(yp,0,1)+0.4*dnorm(yp,3,1))
-
-hist(y,breaks=30,freq=FALSE)
-lines(yp,ghat1,col=2)
-lines(yp,ghat2,col=3)
-f0 <- function(x, y, h) {
- (0.6 * dnorm(x, mean = 0, sd = 1) + 0.4 * dnorm(x, mean = 3, sd = 1))*dnorm(y-x,0,h)
-}
-density_with_error <- sapply(yp, function(y) {
-  integrate(f0, lower = -Inf, upper = Inf, y=y, h=h)$value
-})
-lines(yp,density_with_error)
-
-
+  delta  = matrix(0,nrow=np,ncol=m)
+  for(i in 1:m){
+    delta[yp>tk[i]&yp<=tk[i+1],i] = 2*(yp[yp>tk[i]&yp<=tk[i+1]]-tk[i])^2/d^2/3
+    delta[yp>=tk[i+1]&yp<=tk[i+2],i] = 1-4*(yp[yp>=tk[i+1]&yp<=tk[i+2]]-tk[i+1]-d/2)^2/d^2/3
+    delta[yp>tk[i+2]&yp<=tk[i+3],i] = 2*(yp[yp>tk[i+2]&yp<=tk[i+3]]-tk[i+3])^2/d^2/3
+  }
+  gamma = matrix(0,nrow=np,ncol=m)
+  for(j in 1:m){
+    gamma[,j] = makegamma(yp,tk[j],d, h)
+  }
+  hmat = matrix(0,nrow=m,ncol=m)
+  for(i in 1:m){
+    for(j in 1:i)
+      hmat[i,j] = hmat[j,i]=sum(gamma[,i]*gamma[,j])*(yp[2]-yp[1])
+  }
+  avec = 1:m*0+4*d/3
+  wmat = matrix(0,nrow=m,ncol=m-1)
+  for(i in 1:(m-1)){
+    wmat[i,i] = -1;wmat[i+1,i]=1
+  }
+  b0 = 1:m*0+3/m/d/4
+  cvec = makec(y,tk,d,h)
+  D <- matrix(0,m+1,m)
+  for(i in 1:m){
+    D[i,i]=3
+    D[i+1,i]=-3
+  }
+  for (i in 1:(m-1)) D[i,i+1]=-1
+  for(i in 1:(m-1)) D[i+2,i]=1
+  D = 4*D/3*sqrt(d)
+  ##  get unimodal
+  ans1=deconumfit(y,tk,hmat,cvec,b0,wmat,D,lam,eps=eps,od)
+  ans2=deconbmfit(y,tk,hmat,cvec,b0,wmat,D,lam)
+  t1=as.numeric((ans1$crit-ans2$crit))
+  #t2=as.numeric((ans1$crit-ans2$crit)/abs(ans1$crit))
+  fhat1=round(delta%*%ans1$bhat,10)
+  fhat2=round(delta%*%ans2$bhat,10)
+  # ghat1=round(gamma%*%ans1$bhat,10)
+  # ghat2=round(gamma%*%ans2$bhat,10)
+  dfhat2=diff(fhat2)
+  outtb=NULL
+  
   if(sum(dfhat2>0)==0 |sum(dfhat2<0)==0){
     pvalue=2
   }else{
@@ -161,9 +127,15 @@ lines(yp,density_with_error)
       cdf1=cdf1/cdf1[2000]
       outtb <- foreach(t=1:B,.combine = 'rbind') %dopar%{
         yb=sapply(1:n,function(o){u=runif(1);id=min(which(u<cdf1));alp=(cdf1[id]-u)/(cdf1[id]-cdf1[id-1]);alp*yp[id-1]+(1-alp)*yp[id]})
-        yb=yb+runif(n,-h,h)#;hist(yb,breaks=30)
+        yb=yb+rnorm(n,0,h)#;hist(yb,breaks=30)
         deconmodet(yb,tk,d,h,hmat,b0,wmat,D,delta,lam=lam,eps=eps,od)
-      } 
+      }
+      # outtb <- rep(0,B)
+      # for(t in 1:B){
+      #   yb=sapply(1:n,function(o){u=runif(1);id=min(which(u<cdf1));alp=(cdf1[id]-u)/(cdf1[id]-cdf1[id-1]);alp*yp[id-1]+(1-alp)*yp[id]})
+      #   yb=yb+rnorm(n,0,h)#;hist(yb,breaks=30)
+      #   outtb[t]=deconmodet(yb,tk,d,h,hmat,b0,wmat,D,delta,lam=lam,eps=eps,od)
+      # }
       pvalue=sum(outtb>t1)/B
     }
   }
@@ -183,6 +155,8 @@ lines(yp,density_with_error)
   ans$skewness=skewness(y)
   ans$truncated=(nraw!=n)
   ans
+}
+
 
 ##############
 deconumfit=function(y,tk,hmat,cvec,b0,wmat,D,lam=NULL,eps,od){	
@@ -296,21 +270,316 @@ deconmodet <- function(yb,tk,d,h,hmat,b0,wmat,D,delta,lam,eps,od){
 library(quadprog)
 library(moments)
 library(doParallel)
+no_cores <- detectCores()
+cl <- makeCluster(no_cores - 2)
+registerDoParallel(cl)
+clusterEvalQ(cl, library(quadprog))  # Load the quadprog library in each worker
+clusterExport(cl, list("deconmodetest", "deconumfit", "deconbmfit", "deconmodet", "makec", "makegamma","FUN1","FUN2","FUN3","d1","d2","d3"))
+#stopCluster(cl)
+
 n=800
 h=1
-uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),3,1)
+mu2=4
+uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
 y=x+rnorm(n,0,h)
-ans=deconmodetest(y,h)
+ans=deconmodetest(y,h,v=0.001,B=800)
 ans$pvalue
-hist(y,breaks=40,freq=FALSE)
-lines(ans$yp,ans$ghat1,col=2)
-lines(ans$yp,ans$ghat2,col=3)
-lines(ans$yp,(0.6*pnorm(ans$yp+h,0,1)+0.4*pnorm(ans$yp+h,4,1))/2/h-(0.6*pnorm(ans$yp-h,0,1)+0.4*pnorm(ans$yp-h,4,1))/2/h)
-hist(x,breaks=40,freq=FALSE)
+ans$lam
+ans$crit
+
+
+par(mfrow=c(1,2))
+hist(x,breaks=30,freq=FALSE)
 lines(ans$yp,ans$fhat1,col=2)
 lines(ans$yp,ans$fhat2,col=3)
-lines(ans$yp,0.6*dnorm(ans$yp,0,1)+0.4*dnorm(ans$yp,4,1))
+lines(ans$yp,0.6*dnorm(ans$yp,0,1)+0.4*dnorm(ans$yp,mu2,1))
 
+hist(y,breaks=30,freq=FALSE)
+lines(ans$yp,ans$ghat1,col=2)
+lines(ans$yp,ans$ghat2,col=3)
+f0 <- function(x, y, h) {
+  (0.6 * dnorm(x, mean = 0, sd = 1) + 0.4 * dnorm(x, mean = mu2, sd = 1))*dnorm(y-x,0,h)
+}
+density_with_error <- sapply(ans$yp, function(y) {
+  integrate(f0, lower = -Inf, upper = Inf, y=y, h=h)$value
+})
+lines(ans$yp,density_with_error)
+
+
+#################################
+### simulation for parameters ###
+### v = 0.01, v = 0.001, v = 0.0001
+n=500
+h=1
+mu2=4
+pvmat <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,B=600)
+  ans2=deconmodetest(y,h,v=0.001,B=600)
+  ans3=deconmodetest(y,h,v=0.0001,B=600)
+  pvmat[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat
+#              [,1]        [,2]        [,3]
+#  [1,] 0.048333333 0.068333333 0.125000000
+#  [2,] 0.005000000 0.020000000 0.071666667
+#  [3,] 0.016666667 0.028333333 0.048333333
+#  [4,] 0.035000000 0.153333333 0.255000000
+#  [5,] 0.026666667 0.066666667 0.068333333
+#  [6,] 0.023333333 0.081666667 0.150000000
+#  [7,] 0.013333333 0.010000000 0.025000000
+#  [8,] 0.081666667 0.126666667 0.136666667
+#  [9,] 0.025000000 0.028333333 0.040000000
+# [10,] 0.003333333 0.001666667 0.000000000
+# [11,] 0.000000000 0.005000000 0.020000000
+# [12,] 0.023333333 0.076666667 0.165000000
+# [13,] 0.000000000 0.005000000 0.023333333
+# [14,] 0.050000000 0.138333333 0.228333333
+# [15,] 0.005000000 0.003333333 0.008333333
+# [16,] 0.006666667 0.010000000 0.041666667
+# [17,] 0.008333333 0.026666667 0.053333333
+# [18,] 0.001666667 0.005000000 0.025000000
+# [19,] 0.000000000 0.001666667 0.001666667
+# [20,] 0.010000000 0.030000000 0.021666667
+# [21,] 0.000000000 0.000000000 0.000000000
+# [22,] 0.006666667 0.033333333 0.066666667
+# [23,] 0.020000000 0.036666667 0.070000000
+# [24,] 0.001666667 0.001666667 0.003333333
+# [25,] 0.003333333 0.003333333 0.006666667
+# [26,] 0.081666667 0.216666667 0.296666667
+# [27,] 0.000000000 0.000000000 0.000000000
+# [28,] 0.200000000 0.385000000 0.645000000
+# [29,] 0.015000000 0.038333333 0.025000000
+# [30,] 0.013333333 0.035000000 0.046666667
+# [31,] 0.006666667 0.006666667 0.001666667
+# [32,] 0.018333333 0.008333333 0.001666667
+# [33,] 0.000000000 0.011666667 0.025000000
+# [34,] 0.000000000 0.000000000 0.000000000
+# [35,] 0.000000000 0.000000000 0.000000000
+# [36,] 0.025000000 0.045000000 0.086666667
+# [37,] 0.000000000 0.000000000 0.000000000
+# [38,] 0.000000000 0.000000000 0.003333333
+# [39,] 0.006666667 0.008333333 0.001666667
+# [40,] 0.000000000 0.000000000 0.000000000
+# [41,] 0.223333333 0.358333333 0.558333333
+# [42,] 0.003333333 0.010000000 0.020000000
+# [43,] 0.786666667 0.496666667 0.218333333
+# [44,] 0.005000000 0.016666667 0.041666667
+# [45,] 0.056666667 0.133333333 0.236666667
+# [46,] 0.000000000 0.000000000 0.000000000
+# [47,] 0.061666667 0.115000000 0.153333333
+# [48,] 0.046666667 0.130000000 0.246666667
+# [49,] 0.036666667 0.108333333 0.111666667
+# [50,] 0.001666667 0.011666667 0.020000000
+apply(pvmat,2,function(o){sum(o<=0.05)})
+# 43 35 30
+n=500
+h=1
+mu2=0
+pvmat2 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,B=600)
+  ans2=deconmodetest(y,h,v=0.001,B=600)
+  ans3=deconmodetest(y,h,v=0.0001,B=600)
+  pvmat2[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat2
+# [,1]       [,2]       [,3]
+# [1,]    2 2.00000000 2.00000000
+# [2,]    2 0.30833333 0.23000000
+# [3,]    2 2.00000000 0.63166667
+# [4,]    2 2.00000000 2.00000000
+# [5,]    2 2.00000000 0.54000000
+# [6,]    2 0.14666667 0.13333333
+# [7,]    2 0.27000000 0.21666667
+# [8,]    2 2.00000000 0.71000000
+# [9,]    2 2.00000000 0.30166667
+# [10,]    2 2.00000000 0.76166667
+# [11,]    2 0.08333333 0.04833333
+# [12,]    2 2.00000000 2.00000000
+# [13,]    2 0.57166667 0.47500000
+# [14,]    2 2.00000000 2.00000000
+# [15,]    2 2.00000000 0.40333333
+# [16,]    2 2.00000000 0.56333333
+# [17,]    2 2.00000000 0.57500000
+# [18,]    2 0.53166667 0.48666667
+# [19,]    2 0.24000000 0.25500000
+# [20,]    2 2.00000000 0.63333333
+# [21,]    2 2.00000000 0.52333333
+# [22,]    2 2.00000000 0.64166667
+# [23,]    2 2.00000000 0.63166667
+# [24,]    2 0.22833333 0.12666667
+# [25,]    2 2.00000000 0.48666667
+# [26,]    2 2.00000000 0.20666667
+# [27,]    2 2.00000000 2.00000000
+# [28,]    2 0.44333333 0.32166667
+# [29,]    2 2.00000000 0.63333333
+# [30,]    2 0.27000000 0.33666667
+# [31,]    2 2.00000000 2.00000000
+# [32,]    2 0.28333333 0.20833333
+# [33,]    2 2.00000000 0.18500000
+# [34,]    2 2.00000000 0.73500000
+# [35,]    2 2.00000000 0.81666667
+# [36,]    2 0.36833333 0.39833333
+# [37,]    2 2.00000000 0.45166667
+# [38,]    2 0.17500000 0.16166667
+# [39,]    2 0.60500000 0.57833333
+# [40,]    2 0.35333333 0.42166667
+# [41,]    2 0.57333333 0.58833333
+# [42,]    2 0.44333333 0.59666667
+# [43,]    2 2.00000000 0.45833333
+# [44,]    2 0.52000000 0.48833333
+# [45,]    2 0.51500000 0.70500000
+# [46,]    2 2.00000000 0.36000000
+# [47,]    2 2.00000000 0.75333333
+# [48,]    2 2.00000000 2.00000000
+# [49,]    2 0.18000000 0.08000000
+# [50,]    2 2.00000000 0.79166667
+# 0 0 1
+
+n=500
+h=1
+mu2=4.5
+pvmat3 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,B=600)
+  ans2=deconmodetest(y,h,v=0.001,B=600)
+  #ans3=deconmodetest(y,h,v=0.0001,B=600)
+  pvmat3[irow,] = c(ans1$pvalue, ans2$pvalue, 0)
+}
+pvmat3
+# 49 48
+
+### eps = 1, 2 ; eps = 10. 20; eps = 0.1, 0.2
+n=500
+h=1
+mu2=4
+pvmat4 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,eps1=2,eps2=1,B=600)
+  ans2=deconmodetest(y,h,v=0.01,eps1=1,eps2=0.5,B=600)
+  ans3=deconmodetest(y,h,v=0.01,eps1=0.5,eps2=0.25,B=600)
+  pvmat4[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat4
+# 37 36 36
+n=500
+h=1
+mu2=0
+pvmat5 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,eps1=2,eps2=1,B=600)
+  ans2=deconmodetest(y,h,v=0.01,eps1=1,eps2=0.5,B=600)
+  ans3=deconmodetest(y,h,v=0.01,eps1=0.5,eps2=0.25,B=600)
+  pvmat5[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat5
+# 0 0 0
+
+n=500
+h=1
+mu2=4
+pvmat6 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,B=700)
+  ans2=deconmodetest(y,h,v=0.1,B=700)
+  ans3=deconmodetest(y,h,v=1,B=700)
+  pvmat6[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat6
+apply(pvmat6,2,function(o){sum(o<=0.05)})
+# 34 36 0
+
+n=500
+h=1
+mu2=0
+pvmat7 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,B=700)
+  ans2=deconmodetest(y,h,v=0.1,B=700)
+  ans3=deconmodetest(y,h,v=1,B=700)
+  pvmat7[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat7
+apply(pvmat7,2,function(o){sum(o<=0.05)})
+# 0 0 0
+
+n=500
+h=1
+mu2=4
+pvmat8 <- matrix(0,50,3)
+for(irow in 1:50){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.4]=rnorm(sum(uu<0.4),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans1=deconmodetest(y,h,v=0.01,B=700)
+  ans2=deconmodetest(y,h,v=0.03,B=700)
+  ans3=deconmodetest(y,h,v=0.06,B=700)
+  pvmat8[irow,] = c(ans1$pvalue, ans2$pvalue, ans3$pvalue)
+}
+pvmat8
+apply(pvmat8,2,function(o){sum(o<=0.05)})
+# 0 0 0
+
+
+t1=Sys.time()
+ans1=deconmodetest(y,h,v=0.01,B=600)
+t2=Sys.time()
+t2-t1
+
+
+n=500
+mu2=4
+uu=runif(n);x=rnorm(n,0,1);x[uu<0.5]=rnorm(sum(uu<0.5),mu2,1)
+y=x+rnorm(n,0,h)
+yp=seq(-5,9, length.out = 1000)
+plot(yp,0.5*dnorm(yp,0,1)+0.5*dnorm(yp,mu2,1),type="l")
+yp=seq(1.99,2.01, length.out = 1000)
+h=1.732051
+f0 <- function(x, y, h) {
+  (0.5 * dnorm(x, mean = 0, sd = 1) + 0.5 * dnorm(x, mean = mu2, sd = 1))*dnorm(y-x,0,h)
+}
+density_with_error <- sapply(yp, function(y) {
+  integrate(f0, lower = -Inf, upper = Inf, y=y, h=h)$value
+})
+plot(yp, density_with_error, type="l")
+
+
+n=500
+mu2=4
+
+
+h=0.5 #100/100
+h=1 #95/100
+h = 1.5#29/100
+h=2 #8/200
+pvec = rep(0,1000)
+pvec3 = rep(0,100)
+t1=Sys.time()
+for(i in 1:100){
+  uu=runif(n);x=rnorm(n,0,1);x[uu<0.5]=rnorm(sum(uu<0.5),mu2,1)
+  y=x+rnorm(n,0,h)
+  ans=deconmodetest(y,h,v=0.01,B=1000)
+  pvec3[i]=ans$pvalue
+}
+t2=Sys.time()
+t2-t1
+sum(pvec3<=0.05)
+
+#################################
 
 plot(yp,delta[,1],type = "l")
 for(i in 2:(ncol(delta))){
@@ -321,5 +590,3 @@ plot(yp,gamma[,1],type = "l")
 for(i in 2:ncol(gamma)){
   lines(yp,gamma[,i])
 }
-
-
